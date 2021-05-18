42dfd8fed239ed07a9b809e2b60431e685d3fd15
Gregory Ho <huskit8520g@gmail.com>
2021-05-18 14:08:08 -0400

    Save build files

---
+++ build/precache-manifest.c0972fb00fbe294608f15a23ee23b867.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "970df3c38f2f11c2e41fcf92e53580fc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "cde94ef14242e01b7bfc",
+    "url": "./static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "6052753b97f4ba7e9e20",
+    "url": "./static/css/main.cb37078c.chunk.css"
+  },
+  {
+    "revision": "cde94ef14242e01b7bfc",
+    "url": "./static/js/2.a4ffd431.chunk.js"
+  },
+  {
+    "revision": "1937a0fc6634269e5f47f8181e27fdc7",
+    "url": "./static/js/2.a4ffd431.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6052753b97f4ba7e9e20",
+    "url": "./static/js/main.48fa30c1.chunk.js"
+  },
+  {
+    "revision": "a634487216ccaa40eff1",
+    "url": "./static/js/runtime-main.db838a64.js"
+  },
+  {
+    "revision": "30eb9a50d2d327d7faa61bdceab0f67e",
+    "url": "./static/media/me.30eb9a50.jpg"
+  }
+]);
